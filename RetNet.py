aa3af668160a210c6c9230fc1cac829c02f59e56
Nathan Prisbrey <ntpbanana@gmail.com>
2023-11-16 08:13:16 +0000

    Add Untested FNN Class

--- RetNet.py
+++ RetNet.py
@@ -1,6 +1,36 @@
 import torch
 from torch import nn
 
+class FNN(nn.Module):
+    """ Feed Forward Network """
+    def __init__(self, seq_len: int, d_model: int, fnn_hidden_dim: int=None):
+        """
+        Args:
+            seq_len (int): Size of context window.
+            d_model (int): Embedding dimension size (the "hidden dimension").
+            fnn_hidden_dim (int): Hidden dimension size between the two linear
+                layers. If not set, will use seq_len * d_model * 2.
+        """
+        super().__init__()
+
+        if fnn_hidden_dim is None:
+            fnn_hidden_dim = seq_len * d_model * 2
+
+        self.W_1 = nn.Linear(in_features=seq_len * d_model,
+                             out_features=fnn_hidden_dim,
+                             bias=False)
+        self.W_2 = nn.Linear(in_features=fnn_hidden_dim,
+                             out_features=seq_len * d_model,
+                             bias=False)
+        self.gelu = nn.GELU()
+
+    def forward(self, X: torch.Tensor):
+        # Equation in paragraph below Equation 9 from the paper
+        return self.W_2(self.gelu(self.W_1(nn.Flatten(X)))).reshape(-1,
+                                                                    seq_len,
+                                                                    d_model)
+
+
 class RetNetBlock(nn.Module):
     def __init__(self, seq_len: int, d_model: int):
         """
@@ -11,7 +41,7 @@ class RetNetBlock(nn.Module):
         super().__init__()
 
         self.MSR = MSR()
-        self.FFN = FFN()
+        self.FFN = FFN(seq_len=seq_len, d_model=d_model)
         self.LN = nn.LayerNorm(d_model)
 
     def forward(self, Xsupl: torch.Tensor):
